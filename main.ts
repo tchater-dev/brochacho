833f785c1e96c32b6c9c94a4e087a24c6b059c1f
tchater-dev <t.chater@bthstu.catholic.edu.au>
2026-08-06 16:49:56 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,27 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    statusbar.value += -1
-})
+function SpawnEnemies () {
+    BB01 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . f . . . . . . . . . . . . 
+        . . . . f f f f . . . . . . . . 
+        . . . . . . f f f f f . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f f 2 f f 2 f f . . . 
+        . . . f f f f f f f f f f . . . 
+        . f f f f f f f f f f f f . . . 
+        . f f 2 2 f 2 2 f f f f . . . . 
+        . f f f 2 2 2 f f f . . . . . . 
+        . f f f f f f . . . . . . . . . 
+        . . . f f . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    BB01HP = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
+    BB01HP.attachToSprite(BB01)
+    BB01.follow(mySprite, 45)
+    BB01.setPosition(randint(1, 255), randint(1, 255))
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . 
@@ -21,17 +42,19 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         `, mySprite, 50, 50)
     projectile.follow(BB01)
 })
-statusbars.onZero(StatusBarKind.Health, function (status) {
-    game.gameOver(false)
-})
 statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     sprites.destroy(BB01, effects.fire, 200)
     pause(500)
     game.gameOver(true)
 })
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.gameOver(false)
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     BB01HP.value += -1
-    projectile.setFlag(SpriteFlag.AutoDestroy, false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    statusbar.value += -1
 })
 let projectile: Sprite = null
 let BB01HP: StatusBarSprite = null
@@ -60,26 +83,5 @@ statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.attachToSprite(mySprite)
 controller.moveSprite(mySprite, 50, 50)
 scene.cameraFollowSprite(mySprite)
-BB01 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . f . . . . . . . . . . . . 
-    . . . . f f f f . . . . . . . . 
-    . . . . . . f f f f f . . . . . 
-    . . . . . . f f f f f f . . . . 
-    . . . . f f f 2 f f 2 f f . . . 
-    . . . f f f f f f f f f f . . . 
-    . f f f f f f f f f f f f . . . 
-    . f f 2 2 f 2 2 f f f f . . . . 
-    . f f f 2 2 2 f f f . . . . . . 
-    . f f f f f f . . . . . . . . . 
-    . . . f f . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)
-BB01HP = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
-BB01HP.attachToSprite(BB01)
-BB01.follow(mySprite, 45)
-BB01.setPosition(16, 12)
 tiles.setCurrentTilemap(tilemap`level1`)
+SpawnEnemies()
