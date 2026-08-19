ab193a1431e66d7cd78308148f2e28ef31934654
tchater-dev <t.chater@bthstu.catholic.edu.au>
2026-08-19 13:46:59 +1000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     statusbar.value += -1
 })
+function START_BATTLE (level: number, x: number, y: number) {
+    console.log("activated START_BATTLE")
+    if (level == 1) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    }
+    if (level == 2) {
+        tiles.setCurrentTilemap(tilemap`level0`)
+    }
+    mySprite.setPosition(x, y)
+}
 function SpawnEnemies () {
     BB01 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -83,6 +93,7 @@ mySprite = sprites.create(img`
 statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.attachToSprite(mySprite)
 controller.moveSprite(mySprite, 50, 50)
+let GAMESTATE_BATTLE = 0
+let GAMESTATE_STORE = 1
+START_BATTLE(2, 120, 120)
 scene.cameraFollowSprite(mySprite)
-tiles.setCurrentTilemap(tilemap`level1`)
-SpawnEnemies()
